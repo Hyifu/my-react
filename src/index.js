9dbbc220a6442c83487dc521cb37d300bd791a60
Hyifu <hyifu1990@163.com>
2018-01-09 16:04:18 +0800

    添加 redux-logger

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import createSagaMiddleware from 'redux-saga'
+import logger from 'redux-logger'
 
 import reducers from './reducers'
 import rootSaga from './sagas'
@@ -14,7 +15,7 @@ const sagaMiddleware = createSagaMiddleware()
 const store = createStore(
   reducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(sagaMiddleware)
+  applyMiddleware(sagaMiddleware, logger)
 )
 sagaMiddleware.run(rootSaga)
 
